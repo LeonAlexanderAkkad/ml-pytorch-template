d2da617528be521cafcab071c9646171e3fa0af7
LeonAlexanderAkkad <leon@akkad.at>
2023-08-18 18:57:57 +0200

    Added models file and updated simple dataset

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
-from os import path
+import os
 from glob import glob
 from torch.utils.data import Dataset, Subset
+from torchvision import transforms
+from typing import Optional
 
 
 class TrainingDataset(Dataset):
     """Simple dataset used for training."""
 
-    def __init__(self, directory: str):
-        """Sort and stor all files found in given directory."""
-        self.files = sorted(path.abspath(f) for f in glob(path.join(directory, "**", "*"), recursive=True))
+    def __init__(self, path: str, transform: Optional[transforms.Compose] = None):
+        """Sort and store all files found in given directory."""
+        self.files = sorted(os.path.abspath(f) for f in glob(os.path.join(path, "**", "*"), recursive=True))
+        self.transform = transform
 
     def __getitem__(self, index: int):
         """Returns file given an index."""
