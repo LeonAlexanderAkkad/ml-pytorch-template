82137a7e9a9ab1d2ac34c37d0384fe4fcd9d5ba5
Leon Alexander Akkad <103183844+LeonAlexanderAkkad@users.noreply.github.com>
2024-02-07 18:05:55 +0100

    Added targets

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ import os
 
 from glob import glob
 
+import numpy as np
+
 from torch.utils.data import Dataset, Subset
 from torchvision import transforms
 
@@ -9,23 +11,25 @@ from torchvision import transforms
 class TrainingDataset(Dataset):
     """Simple dataset used for training."""
 
-    def __init__(self, path: str, transform: transforms.Compose | None = None):
+    def __init__(self, data_dir: str, targets_dir: str, transform: transforms.Compose | None = None):
         """Sort and store all files found in given directory."""
-        self.files = sorted(os.path.abspath(f) for f in glob(os.path.join(path, "**", "*"), recursive=True))
+        self.files = sorted(os.path.abspath(f) for f in glob(os.path.join(data_dir, "**", "*"), recursive=True))
+        self.targets = np.genfromtxt(targets_dir, delimiter=",", dtype=int, filling_values=-1)
         self.transform = transform
 
     def __getitem__(self, index: int):
         """Returns file given an index."""
         sample = self.files[index]
+        target = self.targets[index]
 
-        if self.transform:
-            return self.transform(sample), index
+        if self.transform is not None:
+            return index, self.transform(sample), target
 
-        return sample, index
+        return index, sample, target
 
     def __len__(self):
         """Returns the number of files in the dataset."""
-        return len(self.files)
+        return len(self.targets)
 
     def split(self, train_idx, val_idx, test_idx):
         """Splits the dataset into three subsets given the indices."""
